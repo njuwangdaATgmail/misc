218ddb5f60585cb87711e71d3f9f838d9c062526
njuwangdaATgmail <njuwangda@gmail.com>
2018-10-12 18:27:19 +0800

    sync

--- umklapp1d/parquet.f90
+++ umklapp1d/parquet.f90
@@ -8,6 +8,7 @@ MODULE workspace
   REAL(8) :: mixing = 0.2d0
   REAL(8) :: T = 1d0
   REAL(8) :: Vbare(8)          ! bare interactions
+  INTEGER :: outputlevel
   COMPLEX(8), ALLOCATABLE :: Self(:)        ! (-Nw:Nw)
   COMPLEX(8), ALLOCATABLE :: Selfnew(:)     ! (-Nw:Nw)
   COMPLEX(8), ALLOCATABLE :: G(:)           ! (-Nw:Nw)
@@ -37,16 +38,16 @@ SUBROUTINE BSp()
           DO i2=-Nw+1,Nw-1,2
             Rnew(i,j,k,1)=Rnew(i,j,k,1)+Ip(i,i2,k,2)*Xpp(i2,k)*(Ip(i2,j,k,1)+Rp(i2,j,k,1)) &
               &                        +Ip(i,i2,k,1)*Xpp(i2,k)*(Ip(i2,j,k,2)+Rp(i2,j,k,2))
-            Rnew(i,j,k,2)=Rnew(i,j,k,2)+Ip(i,i2,k,2)*Xpp(i2,k)*(Ip(i2,j,k,1)+Rp(i2,j,k,1)) &
-              &                        +Ip(i,i2,k,1)*Xpp(i2,k)*(Ip(i2,j,k,2)+Rp(i2,j,k,2))
+            Rnew(i,j,k,2)=Rnew(i,j,k,2)+Ip(i,i2,k,2)*Xpp(i2,k)*(Ip(i2,j,k,2)+Rp(i2,j,k,2)) &
+              &                        +Ip(i,i2,k,1)*Xpp(i2,k)*(Ip(i2,j,k,1)+Rp(i2,j,k,1))
             Rnew(i,j,k,3)=Rnew(i,j,k,3)+Ip(i,i2,k,4)*Xpp(i2,k)*(Ip(i2,j,k,3)+Rp(i2,j,k,3)) &
               &                        +Ip(i,i2,k,3)*Xpp(i2,k)*(Ip(i2,j,k,4)+Rp(i2,j,k,4))
             Rnew(i,j,k,4)=Rnew(i,j,k,4)+Ip(i,i2,k,4)*Xpp(i2,k)*(Ip(i2,j,k,4)+Rp(i2,j,k,4)) &
               &                        +Ip(i,i2,k,3)*Xpp(i2,k)*(Ip(i2,j,k,3)+Rp(i2,j,k,3))
             Rnew(i,j,k,5)=Rnew(i,j,k,5)+Ip(i,i2,k,6)*Xpp(i2,k)*(Ip(i2,j,k,5)+Rp(i2,j,k,5)) &
               &                        +Ip(i,i2,k,5)*Xpp(i2,k)*(Ip(i2,j,k,6)+Rp(i2,j,k,6))
-            Rnew(i,j,k,6)=Rnew(i,j,k,6)+Ip(i,i2,k,6)*Xpp(i2,k)*(Ip(i2,j,k,5)+Rp(i2,j,k,5)) &
-              &                        +Ip(i,i2,k,5)*Xpp(i2,k)*(Ip(i2,j,k,6)+Rp(i2,j,k,6))
+            Rnew(i,j,k,6)=Rnew(i,j,k,6)+Ip(i,i2,k,6)*Xpp(i2,k)*(Ip(i2,j,k,6)+Rp(i2,j,k,6)) &
+              &                        +Ip(i,i2,k,5)*Xpp(i2,k)*(Ip(i2,j,k,5)+Rp(i2,j,k,5))
             Rnew(i,j,k,7)=Rnew(i,j,k,7)+Ip(i,i2,k,8)*Xpp(i2,k)*(Ip(i2,j,k,7)+Rp(i2,j,k,7)) &
               &                        +Ip(i,i2,k,7)*Xpp(i2,k)*(Ip(i2,j,k,8)+Rp(i2,j,k,8))
             Rnew(i,j,k,8)=Rnew(i,j,k,8)+Ip(i,i2,k,8)*Xpp(i2,k)*(Ip(i2,j,k,8)+Rp(i2,j,k,8)) &
@@ -57,9 +58,9 @@ SUBROUTINE BSp()
     END DO
     Rnew=Rnew*T
     errorbar=maxval(abs(Rnew-Rp))
-    Rp=Rp+mixing*(Rnew-Rp)
+    Rp=Rnew !Rp+mixing*(Rnew-Rp)
     iter=iter+1
-    PRINT*,'@BSp:',iter,errorbar
+    IF(outputlevel==0) PRINT*,'@BSp:',iter,errorbar
   END DO
 END SUBROUTINE
 
@@ -99,9 +100,9 @@ SUBROUTINE BSc()
     END DO
     Rnew=Rnew*T
     errorbar=maxval(abs(Rnew-Rc))
-    Rc=Rc+mixing*(Rnew-Rc)
+    Rc=Rnew !Rc+mixing*(Rnew-Rc)
     iter=iter+1
-    PRINT*,'@BSc:',iter,errorbar
+    IF(outputlevel==0) PRINT*,'@BSc:',iter,errorbar
   END DO
 END SUBROUTINE
 
@@ -155,31 +156,52 @@ SUBROUTINE BSd()
         END DO
       END DO
     END DO
-    Rnew=Rnew*T
+    Rnew=-Rnew*T  ! "-" comes from the fermion loop
     errorbar=maxval(abs(Rnew-Rd))
-    Rd=Rd+mixing*(Rnew-Rd)
+    Rd=Rnew !Rd+mixing*(Rnew-Rd)
     iter=iter+1
-    PRINT*,'@BSd:',iter,errorbar
+    IF(outputlevel==0) PRINT*,'@BSd:',iter,errorbar
   END DO
 END SUBROUTINE
-
 
 SUBROUTINE Dyson()
   USE workspace
   IMPLICIT NONE
-  INTEGER i,j,k
+  INTEGER i,j,k,i2
+  COMPLEX(8) Ga(-Nw:Nw,-Nw:Nw,-Nw:Nw,8)  ! 1PI
+  Ga=0d0
+  DO i=-Nw+1,Nw-1,2
+    DO j=-Nw+1,Nw-1,2
+      DO k=-Nw,Nw,2
+        Ga(i,j,k,:)=-Vbare(:)
+        DO i2=-Nw+1,Nw-1,2
+          Ga(i,j,k,1)=Ga(i,j,k,1)-Vbare(2)*Xpp(i2,k)*(Ip(i2,j,k,1)+Rp(i2,j,k,1))*T &
+            &                    -Vbare(1)*Xpp(i2,k)*(Ip(i2,j,k,2)+Rp(i2,j,k,2))*T
+          Ga(i,j,k,2)=Ga(i,j,k,2)-Vbare(2)*Xpp(i2,k)*(Ip(i2,j,k,2)+Rp(i2,j,k,2))*T &
+              &                  -Vbare(1)*Xpp(i2,k)*(Ip(i2,j,k,1)+Rp(i2,j,k,1))*T
+          Ga(i,j,k,3)=Ga(i,j,k,3)-Vbare(4)*Xpp(i2,k)*(Ip(i2,j,k,3)+Rp(i2,j,k,3))*T &
+              &                  -Vbare(3)*Xpp(i2,k)*(Ip(i2,j,k,4)+Rp(i2,j,k,4))*T
+          Ga(i,j,k,4)=Ga(i,j,k,4)-Vbare(4)*Xpp(i2,k)*(Ip(i2,j,k,4)+Rp(i2,j,k,4))*T &
+              &                  -Vbare(3)*Xpp(i2,k)*(Ip(i2,j,k,3)+Rp(i2,j,k,3))*T
+          Ga(i,j,k,5)=Ga(i,j,k,5)-Vbare(6)*Xpp(i2,k)*(Ip(i2,j,k,5)+Rp(i2,j,k,5))*T &
+              &                  -Vbare(5)*Xpp(i2,k)*(Ip(i2,j,k,6)+Rp(i2,j,k,6))*T
+          Ga(i,j,k,6)=Ga(i,j,k,6)-Vbare(6)*Xpp(i2,k)*(Ip(i2,j,k,6)+Rp(i2,j,k,6))*T &
+              &                  -Vbare(5)*Xpp(i2,k)*(Ip(i2,j,k,5)+Rp(i2,j,k,5))*T
+          Ga(i,j,k,7)=Ga(i,j,k,7)-Vbare(8)*Xpp(i2,k)*(Ip(i2,j,k,7)+Rp(i2,j,k,7))*T &
+              &                  -Vbare(7)*Xpp(i2,k)*(Ip(i2,j,k,8)+Rp(i2,j,k,8))*T
+          Ga(i,j,k,8)=Ga(i,j,k,8)-Vbare(8)*Xpp(i2,k)*(Ip(i2,j,k,8)+Rp(i2,j,k,8))*T &
+              &                  -Vbare(7)*Xpp(i2,k)*(Ip(i2,j,k,7)+Rp(i2,j,k,7))*T
+        END DO
+      END DO
+    END DO
+  END DO
   Selfnew=0d0
   DO i=-Nw+1,Nw-1,2
-    DO k=-Nw,Nw,2
-      j=i+k
-      IF(abs(j)<=Nw) Selfnew(i)=Selfnew(i)+(Ic(i,i,k,8)+Rc(i,i,k,8) &
-        &                                  +Ic(i,i,k,6)+Rc(i,i,k,6) &
-        &                                  +Ic(i,i,k,4)+Rc(i,i,k,4) &
-        &                                  +Ic(i,i,k,2)+Rc(i,i,k,2))*G(j)
-    END DO
     DO j=-Nw+1,Nw-1,2
-      Selfnew(i)=Selfnew(i)+(Ic(i,j,0,4)+Rc(i,j,0,4) &
-        &                   +Ic(i,j,0,1)+Rc(i,j,0,1))*G(j)
+      k=i+j
+      IF(abs(k)<=Nw) Selfnew(i)=Selfnew(i)+(Ga(i,j,k,1)+Ga(i,j,k,4) &
+        &                                  -Ga(i,i,k,8)-Ga(i,i,k,6) &
+        &                                  -Ga(i,i,k,4)-Ga(i,i,k,2))*G(j)
     END DO
   END DO
   Selfnew=Selfnew*T
@@ -274,6 +296,7 @@ SUBROUTINE init()
   READ(10,*) Niter_parquet
   READ(10,*) eps
   READ(10,*) mixing
+  READ(10,*) outputlevel
   CLOSE(10)
   ALLOCATE(Self(-Nw:Nw),Selfnew(-Nw:Nw),G(-Nw:Nw))
   ALLOCATE(Xpp(-Nw:Nw,-Nw:Nw))
@@ -293,10 +316,53 @@ END SUBROUTINE
 SUBROUTINE output()
   USE workspace
   IMPLICIT NONE
-  INTEGER i
+  INTEGER i,j,k,loc(4)
   OPEN(10,FILE='self.dat')
   DO i=1,Nw,2
-    PRINT'(3F12.6)',i*pi*T,Selfnew(i)
+    WRITE(10,'(3f12.6)') i*pi*T,Self(i)
+  END DO
+  CLOSE(10)
+  OPEN(10,FILE='Ipfull.dat')
+  OPEN(11,FILE='Icfull.dat')
+  OPEN(12,FILE='Idfull.dat')
+  OPEN(20,FILE='Imax.dat')
+  DO i=-Nw+1,Nw-1,2
+    DO j=-Nw+1,Nw-1,2
+      DO k=-Nw,Nw,2
+        WRITE(10,'(19f12.6)') i*pi*T,j*pi*T,k*pi*T,Ip(i,j,k,:)
+        WRITE(11,'(19f12.6)') i*pi*T,j*pi*T,k*pi*T,Ic(i,j,k,:)
+        WRITE(12,'(19f12.6)') i*pi*T,j*pi*T,k*pi*T,Id(i,j,k,:)
+      END DO
+    END DO
+  END DO
+  loc=maxloc(abs(Ip))
+  loc(1:3)=loc(1:3)-Nw-1
+  WRITE(20,'(1x,a,3f12.6,1i6,2f12.6)') 'P:',loc(1:3)*pi*T,loc(4),Ip(loc(1),loc(2),loc(3),loc(4))
+  loc=maxloc(abs(Ic))
+  loc(1:3)=loc(1:3)-Nw-1
+  WRITE(20,'(1x,a,3f12.6,1i6,2f12.6)') 'C:',loc(1:3)*pi*T,loc(4),Ic(loc(1),loc(2),loc(3),loc(4))
+  loc=maxloc(abs(Id))
+  loc(1:3)=loc(1:3)-Nw-1
+  WRITE(20,'(1x,a,3f12.6,1i6,2f12.6)') 'D:',loc(1:3)*pi*T,loc(4),Id(loc(1),loc(2),loc(3),loc(4))
+  CLOSE(10)
+  CLOSE(11)
+  CLOSE(12)
+  CLOSE(20)
+END SUBROUTINE
+
+SUBROUTINE exact()
+  USE workspace
+  IMPLICIT NONE
+  INTEGER i
+  REAL(8) U,Z
+  COMPLEX(8) iw,Gexact
+  U=Vbare(7)
+  Z=exp(U/T)+exp(-U/T)+14d0
+  OPEN(10,FILE='selfexact.dat')
+  DO i=1,Nw-1,2
+    iw=uniti*i*pi*T
+    Gexact=((1d0/(iw-U)+1d0/(iw+U))*(2+exp(-U/T)+exp(U/T))/2+12d0/iw)/Z
+    WRITE(10,'(3f12.6)') i*pi*T,iw-1d0/Gexact
   END DO
   CLOSE(10)
 END SUBROUTINE
@@ -312,16 +378,16 @@ PROGRAM parquet
   DO WHILE(errorbar>eps.and.iter<Niter_parquet)
     CALL getX()
     CALL projector()
-    print*,'projector finished'
     CALL BSp()
     CALL BSc()
     CALL BSd()
-    print*,'BS finished'
+    CALL projector()
     CALL Dyson()
     errorbar=maxval(abs(Selfnew-Self))
     Self=Self+mixing*(Selfnew-Self)
     iter=iter+1
-    PRINT*,'@parquet:',iter,errorbar
+    PRINT'(1a,1i6,3f12.6)','@parquet:',iter,errorbar,self(1)
   END DO
   CALL output()
+  CALL exact()
 END PROGRAM
