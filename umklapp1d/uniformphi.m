c2341f401ef31104ebfde87ebb810e033a406d80
njuwangdaATgmail <njuwangda@gmail.com>
2018-10-27 10:26:09 +0800

    sync

--- umklapp1d/uniformphi.m
+++ umklapp1d/uniformphi.m
@@ -7,7 +7,7 @@ Gphi=0.5*(1./(iw0-phi)+1./(iw0+phi));
 Fphi=-2/beta*log(2+2*cosh(beta*phi))+phi.^2;
 Wphi=phi.*exp(-beta*Fphi);
 G0=sum(Gphi.*Wphi)/sum(Wphi);
-Sig0=iw0-1./G0;
+Sig0=iw0-1/G0;
 
 Z=14+2*cosh(beta);
 G0exact=((1/(iw0+1)+1/(iw0-1))*(1+cosh(beta))+12/iw0)/Z;
