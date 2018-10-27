7a75ee6410fdeefaa415da24c082fb588ccd1809
Da Wang <njuwangda@gmail.com>
2018-10-27 09:23:29 +0800

    update uniform.m

--- umklapp1d/uniformphi.m
+++ umklapp1d/uniformphi.m
@@ -1,12 +1,24 @@
-function[G0,Sig0,G0exact,Sig0exact]=uniformphi(beta)
+function[G0,Sig0,G0exact,Sig0exact]=uniformphi(beta,Nk)
+g=1;
 
 dphi=0.01;
-phi=0:dphi:100*sqrt(1/beta);  % |phi|^2>>T*g, where g=1
+phi=0:dphi:100*sqrt(g/beta);  % |phi|^2>>T*g, where g=1
 iw0=1i*pi/beta;
-Gphi=0.5*(1./(iw0-phi)+1./(iw0+phi));
-Fphi=-2/beta*log(2+2*cosh(beta*phi))+phi.^2;
+
+G11=0.5*(1./(iw0-phi)+1./(iw0+phi));
+G12=0.5*(1./(iw0-phi)-1./(iw0+phi));
+
+if Nk==0
+    closed=1;
+    Gphi=G11;
+elseif Nk==1
+    closed=1-0.5*g*beta+0.5*g*beta*tanh(beta*phi/2).^2;
+    Gphi=G11.*closed; %+G11*0.5*g.*G11+G12*0.5*g.*G12+G11.*G12*g.*tanh(beta*phi/2);
+end
+
+Fphi=-2/beta*log(2+2*cosh(beta*phi))+phi.^2/g;
 Wphi=phi.*exp(-beta*Fphi);
-G0=sum(Gphi.*Wphi)/sum(Wphi);
+G0=sum(Gphi.*Wphi)/sum(closed.*Wphi);
 Sig0=iw0-1./G0;
 
 Z=14+2*cosh(beta);
